0d392377d3c25174b62dcfced7a789575ed7100e
vlad <vladpuschiulescu@gmail.com>
2022-11-23 12:23:02 +0200

    Update cors origin

--- Startup.cs
+++ Startup.cs
@@ -38,7 +38,7 @@ namespace bclDetailingBackend
                 .UseSession()
                 .UseRouting()
                 .UseCors(builder => builder
-                    .WithOrigins(new[] { "http://ec2-18-196-89-84.eu-central-1.compute.amazonaws.com" })
+                    .WithOrigins(new[] { "http://bcl-detailing.eu-central-1.elasticbeanstalk.com" })
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials()
